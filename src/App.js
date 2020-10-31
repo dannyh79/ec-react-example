778d54158984a905f123045b83864c7d4baa4ed4
danny-ch han <chenghsuan@5xruby.tw>
2020-10-31 23:16:58 +0800

    Move fetching logic to src/products/index.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { makeClient } from '@spree/storefront-api-v2-sdk';
-import ProductList from './product/ProductList';
+import React from 'react';
+import Products from './products/';
 import './tailwind.output.css';
 
 const App = () => {
-  const [page, setPage] = useState(1);
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const client = makeClient({
-      host: 'http://localhost:3000'
-    });
-
-    (async () => {
-      const productsIndexReq = await client.products.list({
-        include: 'default_variant',
-        page: page
-      });
-
-      if (productsIndexReq.isSuccess) {
-        const data = productsIndexReq.success().data.map(({ id, attributes }) => ({id, ...attributes}));
-        setProducts(data);
-      }
-    })();
-  }, [page])
 
   return (
     <div className="App">
-      <ProductList products={products} />
-      <div className="mt-4 mx-auto text-center">
-        <button onClick={() => setPage(page + 1)}>
-          Next Page
-        </button>
-      </div>
+      <Products />
     </div>
   );
 }
---
+++ src/products/index.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { makeClient } from '@spree/storefront-api-v2-sdk';
+import ProductList from './ProductList';
+
+const Products = () => {
+  const [page, setPage] = useState(1);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const client = makeClient({
+      host: 'http://localhost:3000'
+    });
+
+    (async () => {
+      const productsIndexReq = await client.products.list({
+        include: 'default_variant',
+        page: page
+      });
+
+      if (productsIndexReq.isSuccess) {
+        const data = productsIndexReq.success().data.map(({ id, attributes }) => ({id, ...attributes}));
+        setProducts(data);
+      }
+    })();
+  }, [page])
+
+  return (
+    <>
+      <ProductList products={products} />
+      <div className="mt-4 mx-auto text-center">
+        <button onClick={() => setPage(page + 1)}>
+          Next Page
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Products;
