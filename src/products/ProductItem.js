2b92917ef4502ad4e82a3e4af343609f6a1c5d8e
danny-ch han <chenghsuan@5xruby.tw>
2020-11-02 02:31:56 +0800

    tmp remove description in product card

--- src/products/ProductItem.js
+++ src/products/ProductItem.js
@@ -6,10 +6,8 @@ const ProductItem = ({ product }) => {
   const dispatch = useDispatch();
 
   return (
-    <div
-      className="w-1/4 p-4 border-4 border-double">
+    <div className="w-1/4 p-4 border-4 border-double">
       <h3>{`${product.id}. ${product.name}`}</h3>
-      <p>{product.description}</p>
       <button onClick={() => dispatch(asyncAddItemToCart(product.id))}>
         +
       </button>
